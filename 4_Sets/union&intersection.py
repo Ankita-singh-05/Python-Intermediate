3cfa054f4f29dd28a7957066e44b7f169bed4c26
Ankita-singh-05 <asankitasingh2017@gmail.com>
2022-01-30 19:37:49 +0530

    Union and Difference in set

--- 4_Sets/union&intersection.py
+++ 4_Sets/union&intersection.py
@@ -1,3 +1,21 @@
-odds = set(1,3,5,7,9)
+odds = set([1,3,5,7,9])
 evens = {0,2,4,6,8,10}
 prime = set([2, 3, 5, 7])
+
+# Calculating the unions
+
+u = odds.union(evens)
+# u = odds.union(evens,prime)
+print(u)
+
+i = odds.intersection(prime)
+print(i) 
+# {3,5,7}
+
+i = odds.intersection(evens)
+print(i)
+# set()
+
+i = evens.intersection(prime)
+print(i)
+# {2}
