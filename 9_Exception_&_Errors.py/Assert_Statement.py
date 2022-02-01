0bb830f054ba93746a18e65f6b6ff3acc6a77cb8
Ankita-singh-05 <asankitasingh2017@gmail.com>
2022-02-01 18:53:09 +0530

    Assert and Raise Keyword

---
+++ 9_Exception_&_Errors.py/Assert_Statement.py
@@ -0,0 +1,9 @@
+# AssertionError  -- Assert keyword will throw the assertion error if the assertion is not true
+
+x = -5
+assert (x>=0), "x is not postive"
+
+# if the statement is true then it will not return an error
+
+x = 5
+assert (x>=0), "x is not postive"
---
+++ 9_Exception_&_Errors.py/Raise_Keyword.py
@@ -0,0 +1,10 @@
+# Raise -- If we want to force an exception to occur when a certain condition is met, then we can do this  with the race keyword
+
+x = -5
+if x < 0:
+    raise Exception("x should be +ve")
+
+# If condition is true then error is not shown
+x = 5
+if x < 0:
+    raise Exception("x should be +ve")
