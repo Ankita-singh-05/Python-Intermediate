4ef27b18ed146c0eef5433a4eb511d6ab3ccde32
Ankita-singh-05 <asankitasingh2017@gmail.com>
2022-02-01 18:38:32 +0530

    IndexError and KeyError

---
+++ 9_Exception_&_Errors.py/Exception/indexError.py
@@ -0,0 +1,6 @@
+# IndexError -- It happens when we try to access the index which is out of range
+
+a = [1,2,3]
+a[4]   # Index is upto 2 so it will throw an IndexError
+print(a)
+
---
+++ 9_Exception_&_Errors.py/Exception/keyError.py
@@ -0,0 +1,5 @@
+# KeyError --> It happens when we try to access the key which is not present in the dict
+
+my_dict = {"name": "ankita"}
+# my_dict["age"]  --> KeyError
+print(my_dict)
