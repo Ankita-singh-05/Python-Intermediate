dd9f590a5a61f37580b0d44ba20186e4738765fb
Ankita-singh-05 <asankitasingh2017@gmail.com>
2022-02-02 23:28:14 +0530

    Load

---
+++ 11_JSON.py/json_to_dict.py
@@ -0,0 +1,18 @@
+import json
+
+person = {
+    "name": "John", 
+    "age": 30, 
+    "city": "New York", 
+    "hasChildren": False, 
+    "titles": ["engineer", "programmer"]
+}
+
+personJSON = json.dumps(person, indent=4, sort_keys=True)
+print(personJSON)
+
+with open("person.json", "r") as file:
+    person = json.load(file)
+    print(person)
+
+# {'name': 'John', 'age': 30, 'city': 'New York', 'hasChildren': False, 'titles': ['engineer', 'programmer']}  
