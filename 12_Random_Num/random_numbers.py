b31ed7b22b8e5b6a29cc277888bc42b50d41a9c7
Ankita-singh-05 <asankitasingh2017@gmail.com>
2022-02-03 23:29:18 +0530

    Random Numbers, Random Module

---
+++ 12_Random_Num/random_numbers.py
@@ -0,0 +1,4 @@
+# Random Numbers -
+# RANDOM MODULE for pseudo random numbers, 
+# THE SECRET MODULE for cryptographically strong random numbers
+# NUMPY random to generate arrays with random numbers
