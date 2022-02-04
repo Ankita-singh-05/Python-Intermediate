c44ed5ca741ab0095bcbdcd0bf76d4e737ba1024
Ankita-singh-05 <asankitasingh2017@gmail.com>
2022-02-04 23:41:11 +0530

    Random Seed Method

---
+++ 12_Random_Num/random_secret_method.py
@@ -0,0 +1,2 @@
+#  Seed Method - As the numbers are reproducible in seed method it is not recommeded for the security purpose
+
--- 12_Random_Num/random_seedMethod.py
+++ 12_Random_Num/random_seedMethod.py
@@ -1,5 +1,7 @@
 # Random Seed Method - When we print some random numbers and we want to print the again then it will return the same value
 # Random operation with seed 1 will have the same value and seed 2 will have same value
+# We can reproduce our data with random seed function
+# As this numbers are reproducible it is not recommeded for the security purpose
 import random
 
 random.seed(1)
@@ -9,6 +11,7 @@ print(random.randint(1, 10))
 # 0.13436424411240122
 # 2
 
+# Reseeding it again
 random.seed(1)
 print(random.random())
 print(random.randint(1, 10))
