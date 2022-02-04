c861d8407630bafa23dd54175306392b0dae6afe
Ankita-singh-05 <asankitasingh2017@gmail.com>
2022-02-05 00:36:25 +0530

    Decorators in python

---
+++ 13_Decorators/decorators.py
@@ -0,0 +1,15 @@
+# Decorators - are a very powerful tool in python
+# every advanced python programmer show know it
+
+# Two decorators - 1 . Function decorator
+#                  2 . Class Decorator
+
+
+# Decorator is a function that takes another function as an argument 
+# and extends the behaviour of this function without explicitly modifying it
+# In other words it allows you to add new functionality to an existing function
+
+# Decorator syntax
+@mydecorator
+def dosomething():
+    pass
