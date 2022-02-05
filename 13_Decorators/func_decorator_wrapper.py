97490d58c20d034e5a29127b219808e0a0668603
Ankita-singh-05 <asankitasingh2017@gmail.com>
2022-02-05 23:53:22 +0530

    Decorators in python

---
+++ 13_Decorators/func_decorator_wrapper.py
@@ -0,0 +1,21 @@
+def start_end_decorator(func):
+
+    def wrapper():
+        print("Start")
+        func()
+        print("End")
+    return wrapper
+
+
+def print_name():
+    print("Ankita")
+
+print_name()
+# Ankita
+
+# In order to apply the decorator
+print_name = start_end_decorator(print_name)
+# Start
+# Ankita
+# End
+# 3.18
