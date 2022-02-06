d5e598488df9c6ba9ff7136e96f009999722352a
Ankita-singh-05 <asankitasingh2017@gmail.com>
2022-02-06 16:31:46 +0530

    Function Decorators in python

--- 13_Decorators/func_decorator_wrapper.py
+++ 13_Decorators/func_decorator_wrapper.py
@@ -1,9 +1,10 @@
 def start_end_decorator(func):
 
-    def wrapper():
+    def wrapper(*args, **kwargs):
         print("Start")
-        func()
+        result = func(*args, **kwargs)
         print("End")
+        return result
     return wrapper
 
 
@@ -14,9 +15,31 @@ print_name()
 # Ankita
 
 # In order to apply the decorator
-print_name = start_end_decorator(print_name)
+print_name = start_end_decorator(print_name) 
+# upper line does the same thing as @start_end_decorator
+print_name()
 # Start
 # Ankita
 # End
 
-# 3.18
+@start_end_decorator
+def print_name():
+    print("Ankita")
+print_name()
+# Start
+# Ankita
+# End
+
+
+@start_end_decorator
+def add5(x):
+    return x + 5
+add5(10)
+# Start
+# End
+# If we have to pass the argument in thhe function then we need to define the ars and **kwargs as an argument
+result = add5(10)
+print(result)
+# None 
+# It will return the result after -- result = func(*args, **kwargs)
+# 15
