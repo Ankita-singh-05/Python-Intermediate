e6fb0056a7e46081bffe15b2a234638959570b60
Ankita-singh-05 <68849496+Ankita-singh-05@users.noreply.github.com>
2022-02-11 19:39:53 +0530

    Update Raise_Keyword.py

--- 9_Exception_&_Errors.py/Raise_Keyword.py
+++ 9_Exception_&_Errors.py/Raise_Keyword.py
@@ -1,4 +1,4 @@
-# Raise -- If we want to force an exception to occur when a certain condition is met, then we can do this  with the race keyword
+# Raise -- If we want to force an exception to occur when a certain condition is met, then we can do this  with the raise keyword
 
 x = -5
 if x < 0:
